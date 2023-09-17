b3e8dc2c3232eabf9bab531157dca571a951f609
aswin9677 <aswin96777@gmail.com>
2023-09-17 13:16:57 +0530

    404 added

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
+import PageNotFound from './404/PageNotFound'
+import { Route ,Routes} from 'react-router-dom'
 function App() {
   return (
-    <div className="h-screen  bg-gray-900">
-
-      
-      <Navbar />
+    <div className='h-screen bg-grey-900'>
+    <Routes>
+      <Route path='/navbar' element={<Navbar/>}/>
+      <Route path='/*' element={<PageNotFound />}/>
+    </Routes>
     </div>
   )
 }

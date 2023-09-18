93d75b5fc5c4483478de1c039a3a0743fe4ee74f
aswin9677 <aswin96777@gmail.com>
2023-09-18 22:15:07 +0530

    added login api

--- back-end/server.js
+++ back-end/server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const PORT = process.env.PORT || 9090;
+const url = "mongodb://localhost:27017/data_base";
 
+mongoose.connect(url,{useNewUrlparser:true});
+app.use(express.json())
+const con = mongoose.connection;
 
-app.get("/",(req,res)=>{
-    res.send("hello");
+con.on('open',()=>{
+    console.log("DataBase connected.......");
 })
-
-
-app.listen(2000,()=>{
-    console.log("listening....2000");
+const userRouter = require('./routes/user')
+app.use('/user',userRouter)
+app.listen(9090,()=>{
+    console.log(`server started at ${PORT}` );
 })

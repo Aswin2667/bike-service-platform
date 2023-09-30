cd0615bd2c672c72cf2352b5147d6a3cc28a4244
aswin9677 <aswin96777@gmail.com>
2023-09-30 20:24:33 +0530

    404 page changes

--- front-end/src/pages/404/PageNotFound.jsx
+++ front-end/src/pages/404/PageNotFound.jsx
@@ -4,18 +4,18 @@ const PageNotFound = () => {
   return ( 
     <><div className="w-screen h-screen bg-white">
 
-     <div class="container">
-      <div class="gif">
+     <div className="container">
+      <div className="gif">
         <img src="https://i.postimg.cc/2yrFyxKv/giphy.gif" alt="gif_ing" />
       </div>
-      <div class="content">
-        <h1 class="main-heading" > ERROR 404 - This page is gone.</h1>
+      <div className="content">
+        <h1 className="main-heading" > ERROR 404 - This page is gone.</h1>
         <p>
           ...maybe the page you're looking for is not found or never existed.
         </p>
         <a href="/home" target="blank">
 
-          <button class='button'>Back to home <i class="far fa-hand-point-right"></i></button>
+          <button className='button'>Back to home <i className="far fa-hand-point-right"></i></button>
         </a>
       </div>
     </div>
